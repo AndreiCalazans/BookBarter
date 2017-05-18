6fc7aa2e974fceb163bdbe26ee126ad0c6472bbd
AndreiCalazans <andreixoc@hotmail.com>
2017-05-18 16:32:28 -0300

    problem with react-router

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { Route , Router } from 'react-router-dom';
+import { Route , Switch } from 'react-router-dom';
 import Home from './Home';
 import Books from './Books';
 import Signin from './auth/signin';
 import Signup from './auth/signup';
 import Nav from './Nav';
 class Main extends React.Component {
-
+    
 
     render() {
        
         return (
                 <div>
                     <Nav></Nav>
-                    <Route exact path='/' component={Home}></Route>
-                    <Route path='/signin' component={Signin}></Route>
-                    <Route path='/signup' component={Signup}></Route>
-                    <Route path='/books' component={Books}></Route>
+                    <Switch>
+                        <Route exact path='/' component={Home}></Route>
+                        <Route path='/signin' component={Signin}></Route>
+                        <Route path='/signup' component={Signup}></Route>
+                        <Route path='/books' component={Books}></Route>
+                    </Switch>
                                         
                 </div>
             
--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,19 +1,62 @@
 import React from 'react';
 import {NavLink } from 'react-router-dom';
+import {connect} from 'react-redux';
 
 class Nav extends React.Component {
+    renderLogControls() {
+        if(!this.props.auth.authenticated) {
+            return (
+                <div >
+                        <NavLink  to='/'>Home</NavLink>                        
+                        <NavLink onClick={this.showSlider} to='/signin'>Sign In</NavLink>
+                        <NavLink onClick={this.showSlider} to='/signup'>Sign Up</NavLink> 
+            
+                </div>
+            )
+        } else {
+            return (
+            <div>
+                        <NavLink onClick={this.showSlider} to='/'>Home</NavLink>                        
+                        <NavLink onClick={this.showSlider} to='/signin'>Profile</NavLink>
+                        <NavLink onClick={this.showSlider} to='/signup'>Sign Out</NavLink> 
+                    
+                </div>
+            )
+        }
+    }
+
+    showSlider() {
+        this.refs.slider.classList.toggle('show-slider');
+        this.refs.chevron.classList.toggle('rotate-chevron');
+        
+    }
+
     render() {
         return (
             <div className='Nav'>
-                <NavLink to='/'>Home</NavLink>
-                <hr/>
-                <NavLink to='/signin'>Sign In</NavLink>
-                <NavLink to='/signup'>Sign Up</NavLink>
-                {/*add a profile page */}
-                
+               <div className='top-nav'>
+                   <div className='top-nav_div'>
+                        <p>Logo <i ref='chevron' onClick={() => {this.showSlider()}} className="fa fa-chevron-down hidden-md hidden-lg" aria-hidden="true"></i></p>
+                   </div>
+                   <div className="hidden-xs hidden-sm top-nav_div">
+                        {this.renderLogControls()}
+                   </div>
+               </div> 
+               
+               <div ref='slider' className="navSlider hidden-md hidden-lg">
+                   
+                    {this.renderLogControls()}
+
+               </div> 
             </div>
         )
     }
 }
 
-export default Nav;
+function mapStateToProps(state) {
+    return {
+        auth: state.auth
+    }
+}
+
+export default connect( mapStateToProps )(Nav);
